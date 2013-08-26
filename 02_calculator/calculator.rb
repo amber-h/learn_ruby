06b8186267714fdf4347f2b2fd62cf3d1cfda8ca
amber-h <amber.houle3@gmail.com>
2013-08-26 09:16:47 -0400

    calculator edit

---
+++ 02_calculator/calculator.rb
@@ -0,0 +1,20 @@
+def add(a,b)
+	sum = a+b
+	return sum
+end
+
+def subtract(a,b)
+	difference = a-b
+	return difference
+
+end
+
+def sum(array)
+	sum = 0
+	array.each{ |array_element|
+		sum += array_element
+	}
+	
+	return sum
+
+end
